c36506b8a969958af836d3c3d6546bba493640e9
Ramachandra-2096 <128464954+Ramachandra-2096@users.noreply.github.com>
2023-09-29 11:20:25 +0530

    Update web.py

--- web.py
+++ web.py
@@ -26,4 +26,46 @@ try:
         # Initialize lists for CSS and JavaScript URLs
         css_urls = []
         js_urls = []
+        # Find and extract CSS links from <link> tags
+        link_tags = soup.find_all("link", rel="stylesheet")
+        for link_tag in link_tags:
+            css_url = link_tag.get("href")
+            if css_url:
+                css_urls.append(urljoin(url, css_url))
+
+        # Find and extract JavaScript URLs from <script> tags
+        script_tags = soup.find_all("script", src=True)
+        for script_tag in script_tags:
+            js_url = script_tag.get("src")
+            if js_url:
+                js_urls.append(urljoin(url, js_url))
+
+        # Retrieve CSS content
+        css_code = ""
+        for css_url in css_urls:
+            css_response = requests.get(css_url)
+            if css_response.status_code == 200:
+                css_code += f"/* CSS from {css_url} */\n"
+                css_code += css_response.text + "\n"
+
+        # Retrieve JavaScript content
+        js_code = ""
+        for js_url in js_urls:
+            js_response = requests.get(js_url)
+            if js_response.status_code == 200:
+                js_code += f"/* JavaScript from {js_url} */\n"
+                js_code += js_response.text + "\n"
+
+        # Save the extracted code to text files
+        with open("index.html", "w", encoding="utf-8") as html_file:
+            html_file.write(html_code)
+
+        with open("style.css", "w", encoding="utf-8") as css_file:
+            css_file.write(css_code)
+
+        with open("script.js", "w", encoding="utf-8") as js_file:
+            js_file.write(js_code)
+
+        print("HTML, CSS, and JavaScript code saved to text files.")
+
 
